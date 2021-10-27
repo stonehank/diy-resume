19fa4cf465d7b6fd80c8cf9534f0ac18e493ea14
stonehank <stonehank310@gmail.com>
2021-10-27 23:14:15 +0800

    update public path

--- webpack-config/config.js
+++ webpack-config/config.js
@@ -2,7 +2,7 @@ module.exports={
   "author": "stonehank",
   "authorDescription": "stonehank.github.io",
   "outputPath": "dist",
-  "publicPath": "/diy-resume-live/",
+  "publicPath": "/diy-resume/",
   "deepFolderPrefix": "folder-",
   "notTransPrefix": "not-translate-",
   "notPagePrefix": "_"

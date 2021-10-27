0b6700387e69ca467a9bf32e00d2dcc1110472ae
stonehank <stonehank310@gmail.com>
2021-10-28 00:29:28 +0800

    update default font

--- src/pages/index/src/components/Controller.vue
+++ src/pages/index/src/components/Controller.vue
@@ -52,7 +52,7 @@
             },
         },
         mounted(){
-            this.curSelectFamily='Arial'
+            this.curSelectFamily='fangzheng-songke'
         },
         methods:{
             changeFontFamily(family){
